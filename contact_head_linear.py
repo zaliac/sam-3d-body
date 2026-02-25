40a42c926ee576bcd2c35ad6ab9b4e33911f40dc
zaliac <zalia.cn@gmail.com>
2026-02-24 21:17:53 -0500

    small change on contact_head_linear.py

--- contact_head_linear.py
+++ contact_head_linear.py
@@ -5,7 +5,7 @@ class ContactHead(nn.Module):
     def __init__(self, num_vertices=6890):
         super().__init__()
 
-        self.pool = nn.AdaptiveAvgPool2d(1)   # (B,1280,1,1)
+        self.pool = nn.AdaptiveAvgPool2d(4)   # (B,1280,1,1)
 
         self.channel_reduce = nn.Conv2d(
             1280,
@@ -16,21 +16,21 @@ class ContactHead(nn.Module):
         flattened_dim = 64 * 32 * 32
 
         self.mlp = nn.Sequential(
-            nn.Linear(64*32*32, 10240),
+            nn.Linear(1280*4*4, 10240),      # 4096
             nn.ReLU(),
             nn.Dropout(0.3),
 
-            nn.Linear(10240, 10240),
+            nn.Linear(10240, 4096),
             nn.ReLU(),
             nn.Dropout(0.3),
 
-            nn.Linear(10240, num_vertices)
+            nn.Linear(4096, num_vertices)
         )
 
     def forward(self, x):
-        # x = self.pool(feat)              # (B,1280,1,1) <- (B,1280,32,32)
-        x = self.channel_reduce(x)          # (B,64,32,32)
-        x = x.view(x.size(0), -1)        # (B,1280)
+        x = self.pool(x)              # (B,1280,4,4) <- (B,1280,32,32)
+        # x = self.channel_reduce(x)          # (B,64,32,32)
+        x = x.view(x.size(0), -1)        # (B,1280*4*4)
         x = self.mlp(x)                  # (B,6890) contact_logits
         x = torch.sigmoid(x)             # 0~1  contact_prob
         return x
